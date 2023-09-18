3a1be569b3d0a9b695fa28b5bc90780149da8d30
Josh <joshua.ayres@flagship-group.co.uk>
2023-09-18 20:44:55 +0100

    Add UserService and test mediatr

--- src/MobilitywaysAPI.API/Program.cs
+++ src/MobilitywaysAPI.API/Program.cs
@@ -1,11 +1,16 @@
+using MobilitywayAPI.Shared;
+using MobilitywaysAPI.API.Services;
 using MobilitywaysAPI.Infrastructure.Persistence.DependencyInjection;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlPersistence();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.Load("MobilitywaysAPI.Application")));
 
 var app = builder.Build();
+
+app.MapPost("/api/users/createuser", async (UserDto user, IUserService userService) => await userService.CreateUser(user));
 
 app.Run();
---
+++ src/MobilitywaysAPI.API/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MobilitywayAPI.Shared;
+using MobilitywaysAPI.Application.Users.Commands.CreateUser;
+
+namespace MobilitywaysAPI.API.Services;
+
+public interface IUserService
+{
+    Task CreateUser(UserDto user);
+}
+
+public class UserService : IUserService
+{
+    private readonly IMediator _mediator;
+
+    public UserService(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task CreateUser(UserDto user)
+    {
+        await _mediator.Send(new CreateUserCommand { User = user });
+    }
+}
