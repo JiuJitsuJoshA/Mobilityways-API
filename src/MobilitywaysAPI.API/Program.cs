67e176a18576aeb0f1a6f300195e05ea4635997b
Josh <joshua.ayres@flagship-group.co.uk>
2023-09-18 20:31:41 +0100

    Add MediatR, CreateUserCommand

--- src/MobilitywaysAPI.API/Program.cs
+++ src/MobilitywaysAPI.API/Program.cs
@@ -1,8 +1,10 @@
 using MobilitywaysAPI.Infrastructure.Persistence.DependencyInjection;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlPersistence();
+builder.Services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.Load("MobilitywaysAPI.Application")));
 
 var app = builder.Build();
 
---
+++ src/MobilitywaysAPI.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MobilitywayAPI.Shared;
+using MobilitywaysAPI.Domain;
+using MobilitywaysAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilitywaysAPI.Application.Users.Commands.CreateUser;
+
+public record CreateUserCommand : IRequest
+{
+    public UserDto User { get; set; }
+}
+
+public class Handler : IRequestHandler<CreateUserCommand>
+{
+    private readonly IRepository<User> _userRepository;
+
+    public Handler(IRepository<User> userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
